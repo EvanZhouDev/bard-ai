874953ce28a4565aeb6e24a9ff29d41fa7e674c5
Aldhanekaa <aldhanekadev@gmail.com>
2023-06-18 18:45:04 +0700

    ✨ Typescript Support

---
+++ @types/index.d.ts
@@ -0,0 +1,71 @@
+/// <reference types="typescript" />
+
+declare module "bard-ai" {
+	export interface Session {
+		baseURL: "https://bard.google.com";
+		headers: {
+			"X-Same-Domain": "1";
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36";
+			"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8";
+			Origin: "https://bard.google.com";
+			Referer: "https://bard.google.com/";
+			Cookie: string;
+		};
+	}
+
+	let session: Session;
+	let SNlM0e: string;
+
+	export type images = Array<{
+		tag: string;
+		url: string;
+	}>;
+
+	export type IdsT = {
+		conversationID: string;
+		responseID: string;
+		choiceID: string;
+		_reqID: string;
+	};
+	export type init = (sessionID: string) => Promise<string | Error>;
+
+	type queryBardValidRes = {
+		content: string;
+		images: Array<{
+			tag: string;
+			url: string;
+		}>;
+		ids: IdsT;
+	};
+	export type queryBard = (
+		message: string,
+		ids?: IdsT | Record<string, string>
+	) => Promise<queryBardValidRes | string>;
+
+	export type formatMarkdown = (text: string, images: images) => string;
+
+	export type askAI = (
+		message: string,
+		useJSON: boolean
+	) => Promise<queryBardValidRes | undefined | string>;
+
+	export type Chat = {
+		ids?: IdsT | Record<string, string>;
+
+		ask(
+			message: string,
+			useJSON: boolean
+		): Promise<queryBardValidRes | string>;
+
+		export(): typeof this.ids;
+	};
+
+	interface exportDefaults {
+		askAI: askAI;
+		init: init;
+		Chat: Chat;
+	}
+	const defaults: exportDefaults;
+
+	export default defaults;
+}
