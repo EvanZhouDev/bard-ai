3a4fde2059d23107c482c226bdec430615d0394c
Aldhanekaa <aldhanekadev@gmail.com>
2023-07-16 21:29:24 +0700

    ✨ Images alt, improved image upload buffer validation

---
+++ examples/image.js
@@ -0,0 +1,18 @@
+import Bard from "../index.js";
+
+const bard = new Bard("YOUR_COOKIE_KEY");
+
+const imageUrl =
+    "https://images.unsplash.com/photo-1578973615934-8d9cdb0792b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2578&q=80";
+(async function () {
+    const imageRes = await fetch(imageUrl);
+    const imageBuffer = await imageRes.arrayBuffer();
+
+    console.log(
+        await bard.ask("What flag country is in the photo?", {
+            image: {
+                buffer: imageBuffer,
+            },
+        })
+    );
+})();
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import fs from "fs";
 
 class Bard {
     static JSON = "json";
-    static MD = "markdown"
+    static MD = "markdown";
 
     // ID derived from Cookie
     SNlM0e;
@@ -13,7 +13,7 @@ class Bard {
     // Resolution status of initialization call
     #initPromise;
 
-    #bardURL = "https://bard.google.com"
+    #bardURL = "https://bard.google.com";
 
     // Wether or not to log events to console
     #verbose = false;
@@ -34,14 +34,14 @@ class Bard {
         if (cookie) {
             this.#initPromise = this.#init(cookie);
         } else {
-            throw new Error("Please provide a Cookie when initializing Bard.")
+            throw new Error("Please provide a Cookie when initializing Bard.");
         }
         this.cookie = cookie;
     }
 
     // You can also choose to initialize manually
     async #init(cookie) {
-        this.#verbose && console.log("🚀 Starting intialization")
+        this.#verbose && console.log("🚀 Starting intialization");
 
         // Assign headers
         this.#headers = {
@@ -52,57 +52,71 @@ class Bard {
             "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
             Origin: this.#bardURL,
             Referer: this.#bardURL,
-            Cookie: /^__Secure-[0-9]+PSID=.*$/.test(cookie) ? cookie : `__Secure-1PSID=${cookie};`,
+            Cookie: /^__Secure-[0-9]+PSID=.*$/.test(cookie)
+                ? cookie
+                : `__Secure-1PSID=${cookie};`,
         };
 
         // Attempt to retrieve SNlM0e
         try {
-            this.#verbose && console.log("🔒 Authenticating your Google account")
+            this.#verbose &&
+                console.log("🔒 Authenticating your Google account");
             const SNlM0e = await this.#fetch(this.#bardURL, {
                 method: "GET",
                 headers: this.#headers,
                 credentials: "include",
             })
-                .then(response => response.text())
-                .then(text => text.match(/SNlM0e":"(.*?)"/)[1])
+                .then((response) => response.text())
+                .then((text) => text.match(/SNlM0e":"(.*?)"/)[1]);
 
-            if (!SNlM0e) { // Failure to get SNlM0e from response
-                throw new Error("Could not use your Cookie. Make sure that you copied correctly the Cookie with name __Secure-1PSID exactly. If you are sure your cookie is correct, you may also have reached your rate limit.");
+            if (!SNlM0e) {
+                // Failure to get SNlM0e from response
+                throw new Error(
+                    "Could not use your Cookie. Make sure that you copied correctly the Cookie with name __Secure-1PSID exactly. If you are sure your cookie is correct, you may also have reached your rate limit."
+                );
             }
 
             // Assign SNlM0e and return it
             this.SNlM0e = SNlM0e;
-            this.#verbose && console.log("✅ Initialization finished\n")
+            this.#verbose && console.log("✅ Initialization finished\n");
             return SNlM0e;
-        } catch (e) { // Failure to get server
-            throw new Error("Could not fetch Google Bard. You may be disconnected from internet: " + e);
+        } catch (e) {
+            // Failure to get server
+            throw new Error(
+                "Could not fetch Google Bard. You may be disconnected from internet: " +
+                    e
+            );
         }
     }
 
     async #uploadImage(name, buffer) {
-        this.#verbose && console.log("🖼️ Starting image processing")
-        let size = buffer.buffer.byteLength;
+        this.#verbose && console.log("🖼️ Starting image processing");
+        let size = buffer.byteLength;
         let formBody = [
             `${encodeURIComponent("File name")}=${encodeURIComponent([name])}`,
         ];
 
         try {
-            this.#verbose && console.log("💻 Finding Google server destination")
-            let response = await this.#fetch("https://content-push.googleapis.com/upload/", {
-                method: "POST",
-                headers: {
-                    "X-Goog-Upload-Command": "start",
-                    "X-Goog-Upload-Protocol": "resumable",
-                    "X-Goog-Upload-Header-Content-Length": size,
-                    "X-Tenant-Id": "bard-storage",
-                    "Push-Id": "feeds/mcudyrk2a4khkz",
-                },
-                body: formBody,
-                credentials: "include",
-            });
+            this.#verbose &&
+                console.log("💻 Finding Google server destination");
+            let response = await this.#fetch(
+                "https://content-push.googleapis.com/upload/",
+                {
+                    method: "POST",
+                    headers: {
+                        "X-Goog-Upload-Command": "start",
+                        "X-Goog-Upload-Protocol": "resumable",
+                        "X-Goog-Upload-Header-Content-Length": size,
+                        "X-Tenant-Id": "bard-storage",
+                        "Push-Id": "feeds/mcudyrk2a4khkz",
+                    },
+                    body: formBody,
+                    credentials: "include",
+                }
+            );
 
             const uploadUrl = response.headers.get("X-Goog-Upload-URL");
-            this.#verbose && console.log("📤 Sending your image")
+            this.#verbose && console.log("📤 Sending your image");
             response = await this.#fetch(uploadUrl, {
                 method: "POST",
                 headers: {
@@ -116,12 +130,18 @@ class Bard {
 
             const imageFileLocation = await response.text();
 
-            if (response.status != 200) throw new Error("Failed to retrieve image: " + imageFileLocation);
+            if (response.status != 200)
+                throw new Error(
+                    "Failed to retrieve image: " + imageFileLocation
+                );
 
-            this.#verbose && console.log("✅ Image finished working\n")
+            this.#verbose && console.log("✅ Image finished working\n");
             return imageFileLocation;
         } catch (e) {
-            throw new Error("Could not fetch Google Bard. You may be disconnected from internet: " + e);
+            throw new Error(
+                "Could not fetch Google Bard. You may be disconnected from internet: " +
+                    e
+            );
         }
     }
 
@@ -132,14 +152,16 @@ class Bard {
         // Wait until after init
         await this.#initPromise;
 
-        this.#verbose && console.log("🔎 Starting Bard Query")
+        this.#verbose && console.log("🔎 Starting Bard Query");
 
         // If user has not run init
         if (!this.SNlM0e) {
-            throw new Error("Please initialize Bard first. If you haven't passed in your Cookie into the class, run Bard.init(cookie).");
+            throw new Error(
+                "Please initialize Bard first. If you haven't passed in your Cookie into the class, run Bard.init(cookie)."
+            );
         }
 
-        this.#verbose && console.log("🏗️ Building Request")
+        this.#verbose && console.log("🏗️ Building Request");
         // HTTPS parameters
         const params = {
             bl: "boq_assistant-bard-web-server_20230711.08_p0",
@@ -149,30 +171,29 @@ class Bard {
 
         // If IDs are provided, but doesn't have every one of the expected IDs, error
         const messageStruct = [
-            [
-                this.#context + message
-            ],
+            [this.#context + message],
             null,
-            [null, null, null]
+            [null, null, null],
         ];
 
         if (imageBuffer) {
-            let imageLocation = await this.#uploadImage(`bard-ai_upload`, imageBuffer)
-            messageStruct[0].push(
-                0,
-                null,
-                [
-                    [[imageLocation, 1], "bard-ai_upload"],
-                ],
-            )
+            let imageLocation = await this.#uploadImage(
+                `bard-ai_upload`,
+                imageBuffer
+            );
+            messageStruct[0].push(0, null, [
+                [[imageLocation, 1], "bard-ai_upload"],
+            ]);
         }
 
         if (ids) {
             try {
                 const { conversationID, responseID, choiceID } = ids;
-                messageStruct[2] = [conversationID, responseID, choiceID]
+                messageStruct[2] = [conversationID, responseID, choiceID];
             } catch {
-                throw new Error("Please provide the JSON exported exactly as given.");
+                throw new Error(
+                    "Please provide the JSON exported exactly as given."
+                );
             }
         }
 
@@ -195,10 +216,15 @@ class Bard {
 
         // Encode the data
         const formBody = Object.entries(data)
-            .map(([property, value]) => `${encodeURIComponent(property)}=${encodeURIComponent(value)}`)
+            .map(
+                ([property, value]) =>
+                    `${encodeURIComponent(property)}=${encodeURIComponent(
+                        value
+                    )}`
+            )
             .join("&");
 
-        this.#verbose && console.log("💭 Sending message to Bard")
+        this.#verbose && console.log("💭 Sending message to Bard");
         // Send the fetch request
         const chatData = await this.#fetch(url.toString(), {
             method: "POST",
@@ -206,15 +232,15 @@ class Bard {
             body: formBody,
             credentials: "include",
         })
-            .then(response => {
-                return response.text()
+            .then((response) => {
+                return response.text();
             })
-            .then(text => {
-                return JSON.parse(text.split("\n")[3])[0][2]
+            .then((text) => {
+                return JSON.parse(text.split("\n")[3])[0][2];
             })
-            .then(rawData => JSON.parse(rawData))
+            .then((rawData) => JSON.parse(rawData));
 
-        this.#verbose && console.log("🧩 Parsing output")
+        this.#verbose && console.log("🧩 Parsing output");
         // Get first Bard-recommended answer
         const answer = chatData[4][0];
 
@@ -222,20 +248,20 @@ class Bard {
         const text = answer[1][0];
 
         // Get data about images in that answer
-        const images = answer[4]?.map((x) =>
-        ({
-            tag: x[2],
-            url: x[3][0][0],
-            info: {
-                raw: x[0][0][0],
-                source: x[1][0][0],
-                website: x[1][1],
-                favicon: x[1][3]
-            }
-        })
-        ) ?? [];
+        const images =
+            answer[4]?.map((x) => ({
+                tag: x[2],
+                url: x[3][0][0],
+                info: {
+                    raw: x[0][0][0],
+                    source: x[1][0][0],
+                    alt: x[0][4],
+                    website: x[1][1],
+                    favicon: x[1][3],
+                },
+            })) ?? [];
 
-        this.#verbose && console.log("✅ All done!\n")
+        this.#verbose && console.log("✅ All done!\n");
         // Put everything together and return
         return {
             content: this.#formatMarkdown(text, images),
@@ -254,7 +280,7 @@ class Bard {
             useJSON: false,
             imageBuffer: undefined, // Returns as {extension, filename}
             ids: undefined,
-        }
+        };
 
         // Verify that format is one of the two types
         if (config?.format) {
@@ -266,28 +292,41 @@ class Bard {
                     result.useJSON = false;
                     break;
                 default:
-                    throw new Error("Format can obly be Bard.JSON for JSON output or Bard.MD for Markdown output.");
+                    throw new Error(
+                        "Format can obly be Bard.JSON for JSON output or Bard.MD for Markdown output."
+                    );
             }
         }
 
         // Verify that the image passed in is either a path to a jpeg, jpg, png, or webp, or that it is a Buffer
         if (config?.image) {
-            if (Buffer.isBuffer(config.image)) {
+            if (
+                config.image.buffer &&
+                config.image.buffer instanceof ArrayBuffer
+            ) {
                 result.imageBuffer = config.image.buffer;
-            } else if (typeof config.image === 'string' && /\.(jpeg|jpg|png|webp)$/.test(config.image)) {
-                result.imageBuffer = fs.readFileSync(config.image);
+            } else if (
+                typeof config.image === "string" &&
+                /\.(jpeg|jpg|png|webp)$/.test(config.image)
+            ) {
+                result.imageBuffer = fs.readFileSync(config.image).buffer;
             } else {
-                throw new Error("Provide your image as a file path to a .jpeg, .jpg, .png, or .webp, or a Buffer.")
+                throw new Error(
+                    "Provide your image as a file path to a .jpeg, .jpg, .png, or .webp, or a Buffer."
+                );
             }
         }
 
         // Verify that all values in IDs exist
         if (config?.ids) {
             try {
-                const { conversationID, responseID, choiceID, _reqID } = config.ids;
+                const { conversationID, responseID, choiceID, _reqID } =
+                    config.ids;
                 result.ids = config.ids;
             } catch {
-                throw new Error("Please provide the JSON exported exactly as given.");
+                throw new Error(
+                    "Please provide the JSON exported exactly as given."
+                );
             }
         }
 
@@ -299,19 +338,20 @@ class Bard {
 
         for (let imageData of images) {
             const formattedTag = `!${imageData.tag}(${imageData.url})`;
-            text = text.replace(new RegExp(
-                `(?<!\!)\[${imageData.tag.slice(1, -1)}\]`
-            ), formattedTag);
+            text = text.replace(
+                new RegExp(`(?<!\!)\[${imageData.tag.slice(1, -1)}\]`),
+                formattedTag
+            );
         }
 
         return text;
-    };
+    }
 
     // Ask Bard a question!
     async ask(message, config) {
         let { useJSON, imageBuffer, ids } = this.#parseConfig(config);
         let response = await this.#query(message, { imageBuffer, ids });
-        return useJSON ? response : response.content
+        return useJSON ? response : response.content;
     }
 
     createChat(ids) {
@@ -321,9 +361,12 @@ class Bard {
 
             async ask(message, config) {
                 let { useJSON, imageBuffer } = bard.#parseConfig(config);
-                let response = await bard.#query(message, { imageBuffer, ids: this.ids });
+                let response = await bard.#query(message, {
+                    imageBuffer,
+                    ids: this.ids,
+                });
                 this.ids = response.ids;
-                return useJSON ? response : response.content
+                return useJSON ? response : response.content;
             }
 
             export() {
@@ -331,7 +374,7 @@ class Bard {
             }
         }
 
-        return new Chat()
+        return new Chat();
     }
 }
 
