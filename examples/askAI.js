883de8a3ddcf21ffe307c0463ccfa605f8b16c32
EvanZhouDev <evanzhoudev@gmail.com>
2023-07-16 21:38:02 +0800

    ✨ Added docs

--- examples/askAI.js
+++
@@ -1,5 +0,0 @@
-import Bard, { askAI } from "bard-ai";
-
-await Bard.init("YOUR_COOKIE_KEY");
-
-console.log(await askAI("Hello world!"));
--- examples/chat.js
+++
@@ -1,7 +0,0 @@
-import Bard from "bard-ai";
-
-await Bard.init("YOUR_COOKIE_KEY");
-
-let myConversation = new Bard.Chat();
-console.log(await myConversation.ask("How are you?"));
-console.log(await myConversation.ask("What's the last thing I said?"));
